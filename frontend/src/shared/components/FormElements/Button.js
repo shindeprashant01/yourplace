9b8389d3738b167c4e0fd24303e5d19a9dff4b45
Prashant Prakash Shinde <114857505+shindeprashant01@users.noreply.github.com>
2023-05-14 17:29:32 +0530

    final project

---
+++ frontend/src/shared/components/FormElements/Button.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Button.css';
+
+const Button = props => {
+  if (props.href) {
+    return (
+      <a
+        className={`button button--${props.size || 'default'} ${props.inverse &&
+          'button--inverse'} ${props.danger && 'button--danger'}`}
+        href={props.href}
+      >
+        {props.children}
+      </a>
+    );
+  }
+  if (props.to) {
+    return (
+      <Link
+        to={props.to}
+        exact={props.exact}
+        className={`button button--${props.size || 'default'} ${props.inverse &&
+          'button--inverse'} ${props.danger && 'button--danger'}`}
+      >
+        {props.children}
+      </Link>
+    );
+  }
+  return (
+    <button
+      className={`button button--${props.size || 'default'} ${props.inverse &&
+        'button--inverse'} ${props.danger && 'button--danger'}`}
+      type={props.type}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
+
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
